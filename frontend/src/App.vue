15db55dbce373c6d3125da870fc7b3c33a462277
Aliyev Axmadillo <aaalievvv1@gmail.com>
2026-03-17 20:43:40 +0500

    feat: integrate @nuxt/ui and tailwindcss for enhanced UI components, update App.vue to utilize UApp wrapper, and modify main.ts to include ui plugin for improved application structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <UApp>
+    <HelloWorld />
+  </UApp>
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
+import ui from '@nuxt/ui/vue-plugin'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(ui).mount('#app')
